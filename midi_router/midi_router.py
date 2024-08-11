3d8efad1f032bad5b8276b18db43b726ae6d0a4d
Luke Zulauf <lzulauf@gmail.com>
2024-08-11 10:43:47 -0700

    Move functions around and refactor slightly
    No new functionality, but create_mappings_by_input_port_name how has its own function. Functions moved around so that Run and loop are more easily found at the top of the MidiRouter class.

--- midi_router/midi_router.py
+++ midi_router/midi_router.py
@@ -24,14 +24,56 @@ class MidiRouter:
         self.config = config
         self.incoming_message_queue = queue.Queue()
 
-    def create_receive_message_callback(self, input_port_name):
+    def run(self):
+        identifiers_to_input_port_infos, identifiers_to_output_port_infos = self._get_identifiers_to_port_infos()
+        used_input_long_names, used_output_long_names = self._get_used_port_names(identifiers_to_input_port_infos, identifiers_to_output_port_infos)   
+        input_ports_by_long_name = self._open_input_ports(used_input_long_names)
+        output_ports_by_long_name = self._open_output_ports(used_output_long_names)
+        mappings_by_input_port_name = self._create_mappings_by_input_port_name(identifiers_to_input_port_infos, input_ports_by_long_name)
+        
+        try:
+            while True:
+                self._loop(mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos)
+        finally:
+            for port in itertools.chain(input_ports_by_long_name.values(), output_ports_by_long_name.values()):
+                port.close()
+
+    def _loop(self, mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos):
+        input_port_name, message = self.incoming_message_queue.get()
+        for mapping in mappings_by_input_port_name[input_port_name]:
+            if mapping.to_port == config.PortConstant.ALL: 
+                logger.info(f"to ALL: {message}")
+                for output_port in output_ports_by_long_name.values():
+                    # Don't send messages back to the originating device
+                    if output_port.name != input_port_name:
+                        output_port.send(message)
+            else:
+                output_port = output_ports_by_long_name[identifiers_to_output_port_infos[mapping.to_port.identifier].long_name]                    
+                logger.info(f"to {output_port.name}: {message}")
+                output_port.send(message)
+
+    def _create_mappings_by_input_port_name(self, identifiers_to_input_port_infos, input_ports_by_long_name):
+        mappings_by_input_port_name = {
+            name: []
+            for name in input_ports_by_long_name.keys()
+        }
+        for mapping in self.config.mappings:
+            if mapping.from_port == config.PortConstant.ALL:
+                for mapping_list in mappings_by_input_port_name.values():
+                    mapping_list.append(mapping)
+            else:
+                mappings_by_input_port_name[identifiers_to_input_port_infos[mapping.from_port.identifier].long_name].append(mapping)
+        return mappings_by_input_port_name
+
+
+    def _create_receive_message_callback(self, input_port_name):
         def _receive_message_callback(message):
             self.incoming_message_queue.put(IncomingMessage(input_port_name, message))
         return _receive_message_callback
 
     def _open_input_port(self, long_name):
         try:
-            return mido.open_input(long_name, callback=self.create_receive_message_callback(long_name))
+            return mido.open_input(long_name, callback=self._create_receive_message_callback(long_name))
         except RTMidiSystemError as e:
             logger.warning(repr(e))
 
@@ -89,42 +131,3 @@ class MidiRouter:
                 if isinstance(mapping.to_port, config.PortSpecifier)
             }
         return used_input_long_names, used_output_long_names
-
-    def _loop(self, mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos):
-        input_port_name, message = self.incoming_message_queue.get()
-        for mapping in mappings_by_input_port_name[input_port_name]:
-            if mapping.to_port == config.PortConstant.ALL: 
-                logger.info(f"to ALL: {message}")
-                for output_port in output_ports_by_long_name.values():
-                    # Don't send messages back to the originating device
-                    if output_port.name != input_port_name:
-                        output_port.send(message)
-            else:
-                output_port = output_ports_by_long_name[identifiers_to_output_port_infos[mapping.to_port.identifier].long_name]                    
-                logger.info(f"to {output_port.name}: {message}")
-                output_port.send(message)
-
-    def run(self):
-        identifiers_to_input_port_infos, identifiers_to_output_port_infos = self._get_identifiers_to_port_infos()
-        used_input_long_names, used_output_long_names = self._get_used_port_names(identifiers_to_input_port_infos, identifiers_to_output_port_infos)   
-        input_ports_by_long_name = self._open_input_ports(used_input_long_names)
-        output_ports_by_long_name = self._open_output_ports(used_output_long_names)
-        
-        mappings_by_input_port_name = {
-            name: []
-            for name in input_ports_by_long_name.keys()
-        }
-        for mapping in self.config.mappings:
-            if mapping.from_port == config.PortConstant.ALL:
-                for mapping_list in mappings_by_input_port_name.values():
-                    mapping_list.append(mapping)
-            else:
-                mappings_by_input_port_name[identifiers_to_input_port_infos[mapping.from_port.identifier].long_name].append(mapping)
-
-        try:
-            while True:
-                self._loop(mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos)
-        finally:
-            for port in itertools.chain(input_ports_by_long_name.values(), output_ports_by_long_name.values()):
-                port.close()
-
