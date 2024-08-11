079fa8abee731894eda3201930f54e715e8c53e7
Luke Zulauf <lzulauf@gmail.com>
2024-08-11 15:35:16 -0700

    Detect midi device changes
    This adds asyncio task support to allow us to periodically check the list of midi devices to determine if there's a change. If so, a MidiDeviceChangeException is raised, causing the async runner to cancel and for the app to re-initialize all ports, etc before re-entering the asyncio task logic. Unfortunately there are no midi device change callbacks we can simply subscribe to.
    
    This does make the app much more complicated as we not only have midi inputs coming in on multiple threads, but we also have asyncio cooperating parallelism. But it should make the logic performant while still allowing for periodic midi device change detection.
    
    I also cleaned up the port closing logic in an exception by iteratively tracking ports. that way if an exception is raised in the middle of opening ports, all previously opened ports will be properly closed.

--- midi_router/midi_router.py
+++ midi_router/midi_router.py
@@ -1,3 +1,4 @@
+import asyncio
 import collections
 import itertools
 import logging
@@ -19,38 +20,88 @@ logger = logging.getLogger("midi_router")
 IncomingMessage = collections.namedtuple("IncomingMessage", ["input_port_name", "message"])
 
 
+class MidiDeviceChangeException(Exception):
+    pass
+
+
+# Worst-case time between device change checks is the sleep time plus how long
+# we wait to receive a midi message (plus processing time)
+#
+# Increasing the queue get timeout increases performance by processing events as
+# soon as their availble, but with the tradeoff of checking device changes less
+# frequently.
+EVENT_QUEUE_GET_TIMEOUT = 0.6
+MIDI_DEVICE_CHANGE_CHECK_SLEEP = 0.6
+
+
 class MidiRouter:
     def __init__(self, config):
         self.config = config
         self.incoming_message_queue = queue.Queue()
 
     def run(self):
+        # Every time the midi devices change, re-initialize
+        while True:
+            try:
+                self._run()
+            except MidiDeviceChangeException:
+                logger.warn("Midi Device Change Detected. Re-initializing")
+
+    def _run(self):
         identifiers_to_input_port_infos, identifiers_to_output_port_infos = self._get_identifiers_to_port_infos()
         used_input_long_names, used_output_long_names = self._get_used_port_names(identifiers_to_input_port_infos, identifiers_to_output_port_infos)   
-        input_ports_by_long_name = self._open_input_ports(used_input_long_names)
-        output_ports_by_long_name = self._open_output_ports(used_output_long_names)
-        mappings_by_input_port_name = self._create_mappings_by_input_port_name(identifiers_to_input_port_infos, input_ports_by_long_name)
-        
+
+        input_ports_by_long_name = {}
+        output_ports_by_long_name = {}
         try:
-            while True:
-                self._loop(mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos)
+            for long_name in used_input_long_names:
+                input_ports_by_long_name[long_name] = self._open_input_port(long_name)
+
+            for long_name in used_output_long_names:
+                output_ports_by_long_name[long_name] = self._open_output_port(long_name)
+
+            mappings_by_input_port_name = self._create_mappings_by_input_port_name(identifiers_to_input_port_infos, input_ports_by_long_name)
+            
+            asyncio.run(self._run_async(mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos))
         finally:
             for port in itertools.chain(input_ports_by_long_name.values(), output_ports_by_long_name.values()):
                 port.close()
 
-    def _loop(self, mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos):
-        input_port_name, message = self.incoming_message_queue.get()
-        for mapping in mappings_by_input_port_name[input_port_name]:
-            if mapping.to_port == config.PortConstant.ALL: 
-                logger.info(f"to ALL: {message}")
-                for output_port in output_ports_by_long_name.values():
-                    # Don't send messages back to the originating device
-                    if output_port.name != input_port_name:
-                        output_port.send(message)
+    async def _run_async(self, mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos):
+        monitor_midi_device_change_task = asyncio.create_task(self._monitor_midi_device_changes())
+        process_message_queue_task = asyncio.create_task(self._process_message_queue(mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos))
+        await monitor_midi_device_change_task
+        await process_message_queue_task
+
+    async def _process_message_queue(self, mappings_by_input_port_name, output_ports_by_long_name, identifiers_to_output_port_infos):
+        while True:
+            try:
+                incoming_message = self.incoming_message_queue.get(timeout=EVENT_QUEUE_GET_TIMEOUT)
+            except queue.Empty:
+                pass
             else:
-                output_port = output_ports_by_long_name[identifiers_to_output_port_infos[mapping.to_port.identifier].long_name]                    
-                logger.info(f"to {output_port.name}: {message}")
-                output_port.send(message)
+                input_port_name, message = incoming_message
+                for mapping in mappings_by_input_port_name[input_port_name]:
+                    if mapping.to_port == config.PortConstant.ALL: 
+                        logger.info(f"to ALL: {message}")
+                        for output_port in output_ports_by_long_name.values():
+                            # Don't send messages back to the originating device
+                            if output_port.name != input_port_name:
+                                output_port.send(message)
+                    else:
+                        output_port = output_ports_by_long_name[identifiers_to_output_port_infos[mapping.to_port.identifier].long_name]                    
+                        logger.info(f"to {output_port.name}: {message}")
+                        output_port.send(message)
+            await asyncio.sleep(0)  # Cooperative parallelism
+
+    async def _monitor_midi_device_changes(self):
+        old_port_names = (mido.get_input_names(), mido.get_output_names())
+        while True:
+            new_port_names = (mido.get_input_names(), mido.get_output_names())
+            if old_port_names != new_port_names:
+                raise MidiDeviceChangeException()
+            await asyncio.sleep(MIDI_DEVICE_CHANGE_CHECK_SLEEP)  # Cooperative parallelism plus wait
+
 
     def _create_mappings_by_input_port_name(self, identifiers_to_input_port_infos, input_ports_by_long_name):
         mappings_by_input_port_name = {
@@ -82,18 +133,6 @@ class MidiRouter:
             return mido.open_output(long_name)
         except RTMidiSystemError as e:
             logger.warning(repr(e))
-
-    def _open_input_ports(self, used_input_long_names):
-        return {
-            long_name: self._open_input_port(long_name)
-            for long_name in used_input_long_names
-        }
-
-    def _open_output_ports(self, used_output_long_names):
-        return {
-            long_name: self._open_output_port(long_name)
-            for long_name in used_output_long_names
-        }
 
     def _get_identifiers_to_port_infos(self):
         identifiers_to_input_port_infos = {
